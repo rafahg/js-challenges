3243761001543003986f2c2dbf95079e3bc8679e
Rafael <r.hgar78@gmail.com>
2020-09-13 20:25:51 +0200

    challenge average added

---
+++ average.js
@@ -0,0 +1,12 @@
+// Write function avg which calculates average of numbers in given list.
+
+// Examples :
+
+// (1,2,3) -> 2
+// (4,8,3) -> 5
+
+ const average = () => {
+
+ };
+
+ module.exports = average;
---
+++ average.test.js
@@ -0,0 +1,1 @@
+const average = require('./average');
