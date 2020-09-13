e1b49a213fef3b18b8604681d5f4f16ca35fda42
Rafael <r.hgar78@gmail.com>
2020-09-13 15:37:57 +0200

    challenge: is divisible?

---
+++ canWeDivide.js
@@ -0,0 +1,10 @@
+// Your task is to create functionisDivideBy (or is_divide_by) to check if an integer number is divisible by each out of two arguments.
+
+// examples:
+
+// (12,2,6) -> true
+// (11,3,4) -> false
+
+function isDivisible(number, a, b) {
+
+};
