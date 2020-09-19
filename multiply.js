f75591d77858f64672a7e4a20fea1904ff1bf81b
rafahg <r.hgar78@gmail.com>
2020-09-19 11:51:39 +0100

    challenge multiply with for loop

--- multiply.js
+++ multiply.js
@@ -2,4 +2,16 @@ function multiplyAll (array) {
  return array.reduce((a,b) => a * b);
 }
 
+//another way to do the same.
+
+function multiplyAllWithFor (array) {
+    let sum = 1;
+    for(i=0; i<array.length; i++) {
+        sum = sum * array[i];
+    }
+    return sum;
+}
+
 module.exports = multiplyAll;
+module.exports = multiplyAllWithFor;
+
--- multiply.test.js
+++ multiply.test.js
@@ -1,5 +1,10 @@
 const multiplyAll = require('./multiply');
+const multiplyAllWithFor = require('./multiply');
 
 test('multiply all elements of array', () => {
     expect(multiplyAll([1,2,3,4])).toBe(24);
+})
+
+test('multiply all elements in other way', () => {
+    expect(multiplyAllWithFor([1,2,3,4])).toBe(24);
 })
