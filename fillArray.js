64d84b50fbaa01179f5243c8ac39a9a15c5d9b1e
rafahg <r.hgar78@gmail.com>
2020-09-20 17:43:21 +0100

    fill array challenge complete, test passed

--- fillArray.js
+++ fillArray.js
@@ -1,5 +1,9 @@
 function fillArray(number) {
-
+    let arr = [];
+    for(i=0; i < number; i++) {
+        arr.push(i);
+    }
+    return arr;
 }
 
 module.exports = fillArray;
--- fillArray.test.js
+++ fillArray.test.js
@@ -2,5 +2,5 @@ const { TestScheduler } = require("jest");
 const fillArray = require("./fillArray");
 
 test("it return an array with 0 to N-1 elemements", () =>{
-    expect(fillArray(0)).toBe([])
+    expect(fillArray(3)).toStrictEqual([0,1,2]);
 });
