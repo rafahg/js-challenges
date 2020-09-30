0aaa503624cd1574a709832f6c780db150cd46b8
rafahg <r.hgar78@gmail.com>
2020-09-30 18:41:02 +0100

    multiple sum challenge completed

--- multiplesSum.test.js
+++ multiplesSum.test.js
@@ -1,5 +1,9 @@
 const multiplesSum = require('./multiplesSum')
 
-test('it would return a list', () => {
+test('it would return a sum for first 10 numberss', () => {
     expect(multiplesSum(10)).toBe(23);
 });
+
+test('it would return sum of any number list',() => {
+    expect(multiplesSum(1000)).toBe(233168);
+})
