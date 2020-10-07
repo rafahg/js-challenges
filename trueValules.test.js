56ed132326bff37eca165e67b4657b5935e5baa3
rafahg <r.hgar78@gmail.com>
2020-10-07 21:48:31 +0100

    trueValues challenge completed

--- trueValules.test.js
+++ trueValules.test.js
@@ -3,3 +3,12 @@ const trueValues = require("./trueValues");
 test("it will return the number of values in the array", () => {
     expect(trueValues([true])).toBe(1)
 })
+
+test("it will return the number of values in the array", () => {
+    expect(trueValues([false])).toBe(0)
+})
+
+test("it will return the number of values in the array", () => {
+    expect(trueValues([true,true])).toBe(2)
+})
+
