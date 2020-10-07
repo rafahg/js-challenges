93dc7a828d36cd36ce1083c0990af1bf93ded595
rafahg <r.hgar78@gmail.com>
2020-10-07 20:58:58 +0100

    even fibonnacci second test passed

--- evenFibonacci.test.js
+++ evenFibonacci.test.js
@@ -3,3 +3,7 @@ const evenFibbo = require('./evenFibonacci');
 test('it will return the list of fibbonacci sequence until one number', () =>{
     expect(evenFibbo(10)).toBe(44);
 })
+
+test('it will return the list of fibbonacci sequence until one number', () =>{
+    expect(evenFibbo(400)).toBe(7.456482768619924e+83);
+})
