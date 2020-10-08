52102206adab48f2e5e91db42ab04e6b6de03fb9
rafahg <r.hgar78@gmail.com>
2020-10-08 22:00:42 +0100

    addUpNumbers challenge, second test passed

--- addUpNumbers.test.js
+++ addUpNumbers.test.js
@@ -4,3 +4,8 @@ const addUpNumbers = require('./addUpNumbers')
 test("it will sum 1 number",() =>{
     expect(addUpNumbers(1)).toBe(1)
 })
+
+test("it will sum number 2",() =>{
+    expect(addUpNumbers(2)).toBe(3)
+})
+
