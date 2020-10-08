7a33996bca3015d9d0546adb5bbd508f58a04179
rafahg <r.hgar78@gmail.com>
2020-10-08 22:32:35 +0100

    firstLast challenge, test improved and passed

--- firstLast.test.js
+++ firstLast.test.js
@@ -5,3 +5,7 @@ const firstLast = require("./firstLast")
 test("it will return element", () =>{
     expect(firstLast([1,2])).toStrictEqual([1,2])
 })
+
+test("it will return element", () => {
+    expect(firstLast([1,6,true,"bob"])).toStrictEqual([1,"bob"])
+})
