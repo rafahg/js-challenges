722e4da3e64d3ecb353fa75541dc65d52cdf8e82
rafahg <r.hgar78@gmail.com>
2020-10-09 21:26:58 +0100

    Missing angles challenge, second test passed

--- missingThirdAngle.test.js
+++ missingThirdAngle.test.js
@@ -5,4 +5,8 @@ test("it will return rigth if the angle is equal to 90",() => {
     expect(missingAngles(45, 45)).toBe("rigth")
 })
 
+test("it will return obtuse if the angle is equal to 90",() => {
+    expect(missingAngles(45, 40)).toBe("obtuse")
+})
+
 
