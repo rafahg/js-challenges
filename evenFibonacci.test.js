eb1f6c469bae4c88c76a8afd5ad36d91a8f2243b
rafahg <r.hgar78@gmail.com>
2020-10-13 20:41:31 +0100

    evenFibonacci correctly resolved, test passed

--- evenFibonacci.test.js
+++ evenFibonacci.test.js
@@ -5,5 +5,6 @@ test('it will return the list of fibbonacci sequence until one number', () =>{
 })
 
 test('it will return the list of fibbonacci sequence until one number', () =>{
-    expect(evenFibbo(4000000)).toBe(7.456482768619924e+83);
+    expect(evenFibbo(4000000)).toBe(4613732);
 })
+
