a9790d3af32099eb780e43e533cf378d1a3a022b
rafahg <r.hgar78@gmail.com>
2020-10-18 22:08:08 +0100

    ConvenienceStore totalToPounds method tested

--- convenienceStore.test.js
+++ convenienceStore.test.js
@@ -2,7 +2,6 @@ const Store = require('./convenienceStore')
 /* const totalToPounds = require('./convenienceStore') */
 
 
-
 test('it will return the total amount in the pockets!, in pennies',() =>{
     let store = new Store(); 
     expect(store.change([1,0,0,0])).toBe(25) 
@@ -13,7 +12,7 @@ test('it will return the total amount in the pockets!, in pennies',() =>{
     expect(store.change([4,2,8,120])).toBe(280)
 })
 
-/* test('it will convert the amount in the pockets to pounds!',() =>{
-    expect(totalToPounds(280)).toBe(2.80)
+test('it will convert the amount in the pockets to pounds!',() =>{
+    let store = new Store();
+    expect(store.totalToPounds(280)).toBe(2.80)
 })
- */
