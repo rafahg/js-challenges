87b47fd21443e29840268ec80755626bd5721a9a
rafahg <r.hgar78@gmail.com>
2020-10-18 21:52:21 +0100

    Challenge object to array test added

--- convertObjectArray.js
+++ convertObjectArray.js
@@ -1,8 +1,19 @@
+/* Write a function that converts an object into an array, where each element represents a key-value pair in the form of an array.
 
+Examples
+toArray({ a: 1, b: 2 }) ➞ [["a", 1], ["b", 2]]
 
+toArray({ shrimp: 15, tots: 12 }) ➞ [["shrimp", 15], ["tots", 12]]
 
-function convertObjectArray() {
-    return 1;
+toArray({}) ➞ []
+Notes
+Return an empty array if the object is empty.
+ */
+
+function convertObjectArray(object) {
+   let solution = Object.entries(object);
+   return solution;
+
 }
 
 
--- convertObjectArray.test.js
+++ convertObjectArray.test.js
@@ -1,6 +1,12 @@
 const { TestScheduler } = require('jest');
+const convertObjectArray = require('./convertObjectArray');
 const converObjectArray = require('./convertObjectArray');
 
-test('First test',() =>{
-  expect(converObjectArray()).toBe(1)
+test('Return an array of with pair key, values',() =>{
+  expect(converObjectArray({foo: 1, bar: 2})).toStrictEqual([["foo",1],["bar",2]])
+})
+
+test('Return a bigger array of pair key, values',() =>{
+  let example = {foo:1, bar:2, superBar:3, miniBar:4}
+  expect(convertObjectArray(example)).toStrictEqual()
 })
